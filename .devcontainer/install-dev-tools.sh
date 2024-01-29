befe0637da8e4bbb95d3c3172585068718c0ae44
A <cento@users.noreply.github.com>
2024-01-29 13:53:59 +0000

    Updated docker image to cuda 12.3.1

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,17 +1,22 @@
 # see https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#package-manager-ubuntu-install for latest cuDNN version
-cudnn_ver="8.9.4.*-1+cuda11.8"
+# cudnn_ver="8.9.4.*-1+cuda11.8"
+cudnn_ver="8.9.7.*-1+cuda12.2"
 # update system
 apt update
 apt upgrade -y
 # install Linux tools and Python 3
-apt install software-properties-common wget curl python3-dev python3-pip python3-wheel python3-setuptools -y
+# apt install software-properties-common wget curl python3-dev python3-pip python3-wheel python3-setuptools  -y
+apt install software-properties-common wget curl python3-dev python3-pip python3-wheel python3-setuptools zlib1g git -y
 # install Python packages
 python3 -m pip install --upgrade pip
 pip3 install --user -r .devcontainer/requirements.txt
 # update CUDA Linux GPG repository key
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-dpkg -i cuda-keyring_1.0-1_all.deb
-rm cuda-keyring_1.0-1_all.deb
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+# dpkg -i cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+# rm cuda-keyring_1.0-1_all.deb
+rm cuda-keyring_1.1-1_all.deb
 # install cuDNN
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
